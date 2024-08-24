68c11237e9389bb6b309855b63c654c514d418d4
Tushar <tushargaikwadgithub18@gmail.com>
2024-08-24 18:55:07 +0530

    Added custom QR code with logo feature

--- day1_generateScanner/scanner_generate/lib/main.dart
+++ day1_generateScanner/scanner_generate/lib/main.dart
@@ -1,18 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Package Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        useMaterial3: true,
+      ),
+      home: const QrExample(),
+    );
+  }
+}
+
+class QrExample extends StatefulWidget {
+  const QrExample({super.key});
+
+  @override
+  State<QrExample> createState() => _QrExampleState();
+}
+
+class _QrExampleState extends State<QrExample> {
+  String qrData = '';
+  TextEditingController qrDataController = TextEditingController();
+  final qrKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[50],
+      appBar: AppBar(
+        title: const Text('QR Example'),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              RepaintBoundary(
+                key: qrKey,
+                child: QrImageView(
+                  data: qrData,
+                  version: QrVersions.auto,
+                  size: 235.0,
+                  dataModuleStyle: const QrDataModuleStyle(
+                    dataModuleShape: QrDataModuleShape.circle,
+                    color: Color.fromRGBO(245, 96, 64, 1),
+                  ),
+                  eyeStyle: QrEyeStyle(
+                    eyeShape: QrEyeShape.circle,
+                    color: Colors.green[300],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              TextField(
+                controller: qrDataController,
+                decoration: const InputDecoration(
+                  hintText: 'Enter your data',
+                  border: OutlineInputBorder(
+                    gapPadding: 5.0,
+                    borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                    borderSide: BorderSide(
+                      color: Colors.black,
+                      width: 5.0,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () {
+                  if (qrDataController.text.isEmpty) {
+                    return;
+                  }
+                  setState(() => qrData = qrDataController.text);
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+                child: const Text('Generate QR'),
+              )
+            ],
+          ),
         ),
       ),
     );
